9d62aac2bca9abe22b5c33d2a160442d15138a7f
mariofp77 <mariofp77@gmail.com>
2025-02-25 16:01:02 +0100

    0cb62585b7f2c701fa69ae8cbe2a05b6defd3c59 was not complete

--- src/mlff_qd/preprocessing/generate_mlff_dataset.py
+++ src/mlff_qd/preprocessing/generate_mlff_dataset.py
@@ -23,30 +23,6 @@ from mlff_qd.utils.constants import hartree_bohr_to_eV_angstrom, hartree_to_eV, 
 
 np.set_printoptions(threshold=np.inf)
 
-def center_positions(positions, masses):
-    """
-    Center atomic positions by translating the center of mass (COM) to the origin.
-
-    Parameters:
-        positions (np.ndarray): Atomic positions (num_frames, num_atoms, 3).
-        masses (np.ndarray): Atomic masses (num_atoms).
-
-    Returns:
-        np.ndarray: Centered atomic positions (num_frames, num_atoms, 3).
-    """
-    num_frames, num_atoms, _ = positions.shape
-    com_frames = np.zeros((num_frames, 3))
-
-    # Calculate COM for each frame
-    for frame_idx in range(num_frames):
-        com_frames[frame_idx] = np.sum(
-            positions[frame_idx] * masses[:, np.newaxis], axis=0
-        ) / masses.sum()
-
-    # Subtract COM from all positions
-    centered_positions = positions - com_frames[:, np.newaxis, :]
-    return centered_positions
-
 def align_to_reference(positions, reference_positions):
     """
     Align atomic positions of each frame to a reference structure using least-squares fitting.

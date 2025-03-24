523aa280d043e8c19a34a5a20b320b41612603d5
mariofp77 <mariofp77@gmail.com>
2025-03-24 14:25:07 +0100

    Header cleaed up

--- src/mlff_qd/utils/cluster.py
+++ src/mlff_qd/utils/cluster.py
@@ -1,19 +1,9 @@
 import numpy as np
-import pickle 
-import random
-import matplotlib.pyplot as plt
-import yaml 
-import pprint
-import argparse
-from pathlib import Path
-from periodictable import elements
-from scipy.spatial.transform import Rotation as R
-from scm.plams import Molecule
-from CAT.recipes import replace_surface
+
 from sklearn.cluster import KMeans
 from sklearn.cluster import DBSCAN
 from sklearn.mixture import GaussianMixture
-from sklearn.decomposition import PCA
+
 from ase import Atoms
 
 from mlff_qd.utils.io import save_xyz

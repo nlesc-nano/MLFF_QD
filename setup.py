d1b98565832756a11e80015bf26153be460fb19b
mariofp77 <mariofp77@gmail.com>
2025-05-15 13:28:58 +0200

    55eb1acbf75fd2ad998d42a30d51f336a36253fe introduced a problem in the installation

--- setup.py
+++ setup.py
@@ -1,17 +1,8 @@
 from setuptools import setup, find_packages
-  
+
 setup(
     name="mlff_qd",
     version="0.1.0",
-    packages=find_packages(),  # Look for packages in the root directory
-    entry_points={
-        "console_scripts": [
-            "run-md-opt=postprocessing.run_md_opt:main",
-        ]
-    },
-    install_requires=[
-        # List your dependencies here, e.g., "numpy", "matplotlib", "ase", etc.
-    ],
-    python_requires=">=3.7",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
 )
-

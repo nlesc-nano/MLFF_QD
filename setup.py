nlesc-nano/MLFF_QD

commit 55eb1acbf75fd2ad998d42a30d51f336a36253fe

Added script to perform MDs, GEO_OPT, Dataset evaluation, Active Learning

setup.py
@@ -1,8 +1,17 @@
 from setuptools import setup, find_packages
-
+  
 setup(
     name="mlff_qd",
     version="0.1.0",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    packages=find_packages(),  # Look for packages in the root directory
+    entry_points={
+        "console_scripts": [
+            "run-md-opt=postprocessing.run_md_opt:main",
+        ]
+    },
+    install_requires=[
+        # List your dependencies here, e.g., "numpy", "matplotlib", "ase", etc.
+    ],
+    python_requires=">=3.7",
 )
+

commit d1b98565832756a11e80015bf26153be460fb19b

55eb1acbf75fd2ad998d42a30d51f336a36253fe introduced a problem in the installation

setup.py
@@ -1,17 +1,8 @@
 from setuptools import setup, find_packages
-  
+
 setup(
     name="mlff_qd",
     version="0.1.0",
-    packages=find_packages(),  # Look for packages in the root directory
-    entry_points={
-        "console_scripts": [
-            "run-md-opt=postprocessing.run_md_opt:main",
-        ]
-    },
-    install_requires=[
-        # List your dependencies here, e.g., "numpy", "matplotlib", "ase", etc.
-    ],
-    python_requires=">=3.7",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
 )
-
823661b189effcf0f9e377bf0dcb2c9b246b4481
Enumk <rzainulabideen@gmail.com>
2025-10-14 16:40:52 +0200

    Refactor: move compute_local_descriptors to utils.descriptors

--- src/mlff_qd/preprocessing/consolidate_ter.py
+++ src/mlff_qd/preprocessing/consolidate_ter.py
@@ -69,18 +69,8 @@ from mlff_qd.utils.plots import plot_pca, plot_outliers, plot_final_selection
 
 from mlff_qd.utils.io import save_stacked_xyz
 
+from mlff_qd.utils.descriptors import compute_local_descriptors
 
-def compute_local_descriptors(positions, atom_types, soap):
-    print("[SOAP] Computing descriptors...")
-    desc=[]
-    for i in range(positions.shape[0]):
-        if i%100==0: print(f" SOAP frame {i+1}/{positions.shape[0]}")
-        A = Atoms(symbols=atom_types, positions=positions[i], pbc=False)
-        S = soap.create(A).mean(axis=0)
-        desc.append(S)
-    desc=np.array(desc)
-    print(f"[SOAP] Done, shape={desc.shape}")
-    return desc
 
 def analyze_reference_forces(forces, atom_types):
     """
